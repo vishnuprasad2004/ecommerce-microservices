b1a96586615045d7b156cfbdce646c983bc903ac
vishnuprasad2004 <vishnuprasadkorada@gmail.com>
2026-02-02 23:40:15 +0530

    update: all the endpoints added

--- order-service/src/routes/routes.ts
+++ order-service/src/routes/routes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
-import { createOrder, getOrder, listOrders } from "../controllers/orders.controller.js";
+import { createOrder, 
+  getOrder, 
+  getOrderStats, 
+  getUserOrders, 
+  listOrders, 
+  updateOrderStatus, 
+  deleteOrder, 
+} from "../controllers/orders.controller.js";
 
 const router = Router();
 
 router.get("/list", listOrders);
+router.get('/stats', getOrderStats);
 router.get("/:id", getOrder);
+router.get('/user/:userId', getUserOrders);
+
+router.patch("/:id", updateOrderStatus);
 
 router.post("/", createOrder);
 
-
+router.delete("/:id", deleteOrder);
 
 
 export default router;
